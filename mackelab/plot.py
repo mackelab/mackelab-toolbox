7507cb7b21a8c87471913e1678de14edc7ae3db6
Alexandre René <alexandre.rene@freeneurons.ca>
2017-11-21 17:38:29 +0100

    [plot] Add convenience plotting function for sets of 1D data.

--- mackelab/plot.py
+++ mackelab/plot.py
@@ -1,7 +1,14 @@
 import datetime
+from collections import namedtuple, Callable
+import numpy as np
 import matplotlib.pyplot as plt
 
 from subprocess import check_output
+
+from parameters import ParameterSet
+
+###############################
+# Archival helper functions
 
 def saverep(basename, comment=None, pdf=True, png=True):
     """Helper function for figures
@@ -43,3 +50,104 @@ def saverep(basename, comment=None, pdf=True, png=True):
 
     with open(basename + '.txt', 'w') as textfile:
         textfile.write(info_str)
+
+
+# ==================================
+# Plotting
+
+def cleanname(_name):
+    s = _name.strip('$')
+    # wrap underscored elements with brackets
+    s_els = s.split('_')
+    s = s_els[0] + ''.join(['_{' + el + '}' for el in s_els[1:]])
+    # wrap the whole string in brackets, to allow underscore with component
+    return '{' + s + '}'
+
+def plot(data, **kwargs):
+    """
+    Function for quickly plotting multiple 1D data: will automatically generate
+    labels (which you can display with `plt.legend()`). Designed for
+    quick scripts where you don't want to spend time defining the labels yourself.
+
+    If 'data' is not a Numpy array, just checks if it has a '.plot()' method and
+    tries to call that, passing the keyword arguments.
+
+    Parameters
+    ----------
+    data: The data structure to plot. If the data is an numpy array, it is
+        plotted with matplotlib.pyplot.plot. Otherwise, we check to see if
+        the object provides a 'plot' method, and use that. If non is found,
+        a ValueError is raised.
+
+    **kwargs: Most keyword arguments are passed to the plotting function,
+        however the following extra keywords are provided for numpy data:
+          - `label`
+            Can be specified as a single string or a list of strings.
+            In the former case, a subscript is added to indicate components;
+            in the latter,  strings are used as-is and the list should be of
+            the same length as the number of components.
+            If not specified, 'y' is used as a label, with
+            components indicated as a subscript.
+          - `component`
+            Restrict plotting to the specified components.
+        For non-Numpy data, these keywords may also be provided by its `plot`
+        method.
+    Returns
+    -------
+    The created axis (ndarray data). If data provides its own `plot` method,
+    returns its return value.
+
+    """
+    # TODO?: Collect code repeated in methods into reusable blurbs.
+
+    if isinstance(data, np.ndarray):
+        comp_list = kwargs.pop('component', None)
+        label = kwargs.pop('label', None)
+
+        if comp_list is None:
+            if len(data.shape) > 1:
+                comp_list = list( itertools.product(*[range(s) for s in data.shape[1:]]) )
+        else:
+            # Component list must be a list of tuples
+            if not isinstance(comp_list, collections.Iterable):
+                comp_list = [(comp_list,)]
+            elif not isinstance(comp_list[0], collections.Iterable):
+                if isinstance(comp_list, list):
+                    comp_list = [tuple(c) for c in comp_list]
+                elif isinstance(comp_list, tuple):
+                    comp_list = [comp_list]
+                else:
+                    comp_list = [tuple(c) for c in comp_list]
+
+        if comp_list is not None:
+            if label is None or isinstance(label, str):
+                name = label if label is not None else "y"
+                # Loop over the components
+                #if len(comp_list) > 1:
+                labels = [ "${}_{{{}}}$".format(cleanname(name), str(comp).strip('(),'))
+                           for comp in comp_list ]
+                #else:
+                #    labels = [ "${}$".format(cleanname(data.name)) ]
+            else:
+                assert(isinstance(label, collections.Iterable))
+                labels = label
+
+        ax = plt.gca()
+        # Loop over the components, plotting each separately
+        # Plotting separately allows to assign a label to each
+        if comp_list is None:
+            plt.plot(np.arange(len(data)), data)
+
+        else:
+            for comp, label in zip(comp_list, labels):
+                idx = (slice(None),) + comp
+                plt.plot(np.arange(len(data)), data[idx], label=label, **kwargs)
+        return ax
+
+    elif hasattr(data, 'plot'):
+        return data.plot(**kwargs)
+
+    else:
+        logger.warning("Plotting of {} data is not currently supported."
+                       .format(type(data)))
+
