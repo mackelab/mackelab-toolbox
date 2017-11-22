2259edc8e7df37365196715a36eb81bbeb365d44
Alexandre René <alexandre.rene@freeneurons.ca>
2017-11-22 17:35:35 +0100

    Update setup.py. Add entry points for smt file management.
    More complete dependencies. Two optional dependency sets: 'smt' and 'pymc3'.
    Update version number to 0.1.0dev.

--- setup.py
+++ setup.py
@@ -2,8 +2,32 @@ from setuptools import setup
 
 setup(
     name='mackelab',
-    version='0.0.1',
+    version='0.1.0dev',
     description='Common utils for Mackelab',
     url='https://github.com/mackelab/mackelab',
-    install_requires=['matplotlib']
+    install_requires=[
+        'matplotlib',
+        'theano_shim >= 0.2',
+        'simpleeval'
+    ],
+    dependency_links=[
+        'git+ssh://git@github.com:alcrene/parameters.git',
+    ],
+
+    extras_require = {
+        'smt': [
+            'Click',
+            'sumatra'
+        ],
+        'pymc3': [
+            'pymc3'
+        ],
+        'all': [
+            'Click', 'sumatra', 'pymc3'
+        ]
+    },
+    entry_points = {
+        'console_scripts':
+            ['smttk = mackelab.smttk:cli [smt]']
+    }
 )
