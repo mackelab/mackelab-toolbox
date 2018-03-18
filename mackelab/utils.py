3473eff3057234bd855317404e95e33c91409f83
Alexandre René <alexandre.rene@freeneurons.ca>
2018-03-18 18:23:26 +0100

    [utils] Add SanitizedOrderedDict
    SanitizedOrderedDict subclasses OrderedDict, and adds the ability to sanitize
    keys. E.g. if entries are keyed by display text, this may contain formatting
    characters or codes codes which are difficult to retype from memory. This class
    allows this keys to be 'sanitized', i.e. for a set of characters to be removed
    before they are used to index the dictionary.
    Alternatively, an arbitrary sanitization function can also be provided.
    This is intended for user-facing dictionaries, to allow the user to access
    entries without needing to remember the exact key, as long as the one they
    specify is "close enough".

--- mackelab/utils.py
+++ mackelab/utils.py
@@ -8,7 +8,7 @@ Created on Tue Nov 28 2017
 @author: alex
 """
 
-import collections
+from collections import Iterable, Callable, OrderedDict
 
 def flatten(l, terminate=()):
     """
@@ -25,15 +25,71 @@ def flatten(l, terminate=()):
         not expanded.
     """
     # Normalize `terminate` argument
-    if not isinstance(terminate, collections.Iterable):
+    if not isinstance(terminate, Iterable):
         terminate = (terminate,)
     else:
         terminate = tuple(terminate)
     # Flatten `l`
     for el in l:
-        if (isinstance(el, collections.Iterable)
+        if (isinstance(el, Iterable)
             and not isinstance(el, (str, bytes) + terminate)):
             yield from flatten(el, terminate=terminate)
         else:
             yield el
 
+class SanitizedOrderedDict(OrderedDict):
+    """
+    Subclass of OrderedDict with sanitized keys.
+    Any key query is passed through a user-defined 'sanitization' function
+    before searching for a match in the dictionary.
+
+    This is intended for user-facing dictionaries, to allow the user to access
+    entries without needing to remember the exact key, as long as the one they
+    specify is "close enough".
+    """
+
+    def __init__(self, *args, sanitize, ignore_whitespace=True, **kwargs):
+        """
+        Parameters
+        ----------
+        *args, **kwargs: Same as for OrderedDict
+        sanitize: function, or list of characters
+            If a function, used to sanitize inputs.
+            If a list of characters, sanitization is to remove all characters
+            in that list.
+        ignore_whitespace: bool
+            Whether to also ignore all whitespace. Ignored is `sanitize` is
+            a function.
+        """
+        if isinstance(sanitize, Callable):
+            self.sanitize = sanitize
+        elif isinstance(sanitize, Iterable):
+            def f(s):
+                for c in sanitize:
+                    s = s.replace(c, '')
+                if ignore_whitespace:
+                    # Remove all whitespace
+                    s = ''.join(s.split())
+                return s
+            self.sanitize = f
+
+        return super().__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, Iterable) and not isinstance(key, (str, bytes)):
+            key = type(key)([self.sanitize(k) for k in key])
+        else:
+            key = self.sanitize(key)
+        return super().__setitem__(key, value)
+    def __getitem__(self, key):
+        if isinstance(key, Iterable) and not isinstance(key, (str, bytes)):
+            key = type(key)([self.sanitize(k) for k in key])
+        else:
+            key = self.sanitize(key)
+        return super().__getitem__(key)
+
+    def newdict(self, *args, **kwargs):
+        """
+        Return an empty OrderedDict with the same sanitization.
+        """
+        return SanitizedOrderedDict(*args, sanitize=self.sanitize, **kwargs)
