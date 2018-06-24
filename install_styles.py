1dbd3c15b5359d327fcf332b3dc0158dd288ef74
Alexandre René <alexandre.rene@freeneurons.ca>
2018-06-24 10:39:51 +0200

    [setup] Fix install styles
    - Works when run from same directory
    - Puts style fils in 'stylelib' subdirectory

--- install_styles.py
+++ install_styles.py
@@ -10,17 +10,20 @@ import matplotlib
 configdir = matplotlib.get_configdir()
 os.makedirs(configdir, exist_ok=True)  # Make sure directory exists
 
-# Get this module's path
-package_root = os.path.dirname(sys.modules[__name__].__file__)
+# Get the directory containing thi module
+package_root = os.path.dirname(os.path.abspath(sys.modules[__name__].__file__))
 
 # Add directory structure to get path to `stylelib`
 stylelib_path = os.path.join(package_root, 'mackelab/stylelib')
+
+# Make sure the "stylelib" directory exists in the global config folder
+os.makedirs(os.path.join(configdir, "stylelib"), exist_ok=True)
 
 # Loop over all style files and add link in matplotlib config
 for stylefile in os.listdir(stylelib_path):
     if stylefile[-9:] == '.mplstyle':
         try:
             os.symlink(os.path.join(stylelib_path, stylefile),
-                       os.path.join(configdir, stylefile))
+                       os.path.join(configdir, "stylelib", stylefile))
         except FileExistsError:
             pass
